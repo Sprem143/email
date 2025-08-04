a8d862cd3581eaee8ca7c8295fe2d7cddd4f7aa7
Prem Kumar <prem68265@gmail.com>
2025-08-04 22:54:39 +0545

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 import Header from './Header.jsx'
+import Login from './components/Login.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { AuthProvider } from './AuthContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header/>
-    <App />
+   <AuthProvider>
+     <BrowserRouter>
+     <Header/>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<App/>} />
+        <Route path="*" element={<h2>404 Not Found</h2>} />
+      </Routes>
+    </BrowserRouter>
+   </AuthProvider>
   </StrictMode>,
 )
